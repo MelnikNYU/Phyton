de51913b6fd10e1149ae32658487e880fa0280f8
Nadezhda <melnik.nadezhdayu@yandex.ru>
2023-06-05 21:54:25 +0300

    changes

--- HW1/Test 38.py
+++ HW1/Test 38.py
@@ -95,3 +95,108 @@ while action != "s":
             change_records(path)
         elif action == "5":
             delete_records(path)
+
+
+path = "phone_book.txt"
+def print_records(file_name: str):
+    with open(file_name, 'r', encoding='utf-8') as data:
+        for line in data:
+            print(*line.split(';'), end='')
+
+def input_records(file_name: str):
+    with open(file_name, 'r+', encoding='utf-8') as data:
+        record_id = 0
+        for line in data:
+            if line != '':
+                record_id = line.split(';', 1)[0]
+        print('Enter FCS thru space')
+        line = f'{int(record_id) + 1};' + ';'.join(input().split()[:4]) + ';\n'
+        confirm = confirmation('add record')
+        if confirm == 'y':
+            data.write(line)
+
+def find_char():
+    print('Choose char: ')
+    print('0 - id, 1 - second name, 2 - name, 3 - third name, 4 - number, s - stop')
+    char = input()
+    while char not in ('0', '1', '2', '3', '4', 's'):
+        print('Data incorrect')
+        print('Choose char: ')
+        print('0 - id, 1 - second name, 2 - first name, 3 - third name, 4 - number, s - stop')
+        char = input()
+    if char != 's':
+        inp = input('inter value\n')
+        return char, inp
+    else:
+        return 's', 's'
+
+def find_records(file_name: str, char, condition):
+    if condition != 's':
+        printed = False
+        with open(file_name, 'r', encoding='utf-8') as data:
+            for line in data:
+                if condition == line.split(';')[int(char)]:
+                    print(*line.split(';'))
+                    printed = True
+        if not printed:
+            print("Nothing found")
+        return printed
+
+def check_id_record(file_name: str, text: str):
+    decision = input(f'Do you know? {text}? 1 - yes, 2 - no, s -stop\n')
+    while decision not in ('1', 's'):
+        if decision != '2':
+            print('Data incorrect')
+        else:
+            find_records(path, *find_char())
+        decision = input(f'Do you know? {text}? 1 - yes, 2 - no, s -stop\n')
+    if decision == '1':
+        record_id = input('enter id, s - stop\n')
+        while not find_records(file_name, '0', record_id) and record_id != 's':
+            record_id = input('enter id, s - stop\n')
+        return record_id
+    return decision
+
+def confirmation(text: str):
+    confirm = input(f"Confirm {text} record: y - yes, n - no\n")
+    while confirm not in ('y', 'n'):
+        print('Data incorrect')
+        confirm = input(f"Confirm {text} record: y - yes, n - no\n")
+    return confirm
+
+def replace_record_line(file_name: str, record_id, replaced_line: str):
+    replaced = ''
+    with open(file_name, 'r', encoding='utf-8') as data:
+        for line in data:
+            replaced += line
+            if record_id == line.split(';', 1)[0]:
+                replaced = replaced.replace(line, replaced_line)
+    with open(file_name, 'w', encoding='utf-8') as data:
+        data.write(replaced)
+
+def change_records(file_name: str):
+    record_id = check_id_record(file_name, 'change')
+    if record_id != 's':
+        replaced_line = f'{int(record_id)};' + ';'.join(
+            input('Enter\n').split()[:4]) + ';\n'
+        confirm = confirmation('change')
+        if confirm == 'y':
+            replace_record_line(file_name, record_id, replaced_line)
+
+def delete_records(file_name: str):
+    record_id = check_id_record(file_name, 'delete')
+    if record_id != 's':
+        confirm = confirmation('change')
+        if confirm == 'y':
+            replace_record_line(file_name, record_id, '')
+
+def start ():
+    path = 'phone_book.txt'
+
+    try:
+        file = open(path, 'r')
+    except IOError:
+        print('New file created -> phone_book.txt ')
+        file = open(path, 'w')
+    finally:
+        file.close()
